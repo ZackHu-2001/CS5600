2809bedefdf4e9d883f48897011101858e7856ab
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-09-18 15:43:19 -0700

    Upload homework for the first week

---
+++ common.h
@@ -0,0 +1,23 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+// basic head files
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+
+#include <errno.h>
+
+// IO related 
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <math.h>
+#include <dirent.h>
+
+#endif

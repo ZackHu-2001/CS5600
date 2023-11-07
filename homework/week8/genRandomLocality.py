c397326d3cabed3f39b868e14ec7ea62e05e6275
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-11-06 16:34:43 -0800

    add week8's homework

---
+++ homework/week8/genRandomLocality.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+import random 
+Max =  10
+num =  20
+randCite = 0.6
+randomHotPage = 0.2
+
+arr = []
+
+numlist = set(range(0, Max))
+hotPage = set()
+coldPage = set()
+
+while len(hotPage) < int(Max * randomHotPage):
+    i = random.choice(list(numlist))
+    hotPage.add(i)
+coldPage = numlist - hotPage
+
+print(hotPage)
+print(coldPage)
+
+hotPage = list(hotPage)
+coldPage = list(coldPage)
+
+for i in range(0, num):
+    if(random.random() > randCite):
+        t = random.choice(coldPage)
+    else:
+        t = random.choice(hotPage)
+    arr.append(t)
+print(arr)
+
+file = open('./random.txt', 'w')
+
+for i in arr:
+    file.write(str(i) + '\n')
+
+file.close()
